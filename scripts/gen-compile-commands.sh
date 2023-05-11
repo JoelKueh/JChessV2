b95ac49d95e7e9cc065f404de749462409f89711
Joel Kuehne <joelckuehne@gmail.com>
2023-05-11 10:19:36 -0500

    Moved bear command from build to gen-compile-commands script

--- scripts/gen-compile-commands.sh
+++ scripts/gen-compile-commands.sh
@@ -1,9 +1,6 @@
-mkdir out
-rm out/JChessV2
-g++ JChessV2.cpp \
+bear -- g++ JChessV2.cpp \
   scenes/*.cpp \
   scenes/Game/*.cpp scenes/Game/UI/*.cpp scenes/Game/Player/*.cpp \
   scenes/StartMenu/*.cpp \
   chessboard/*.cpp chessboard/move_gen/*.cpp \
--lncursesw -lglfw -lX11 -lGL -g -o out/JChessV2
-./out/JChessV2
+  -lncursesw -l:libglfw3.a -lX11 -lGL -g -o out/JChessV2
