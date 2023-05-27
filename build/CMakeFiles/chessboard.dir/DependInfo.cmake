ba4589248285e482b30fb06a963f6ff872010a35
Joel Kuehne <joelckuehne@gmail.com>
2023-05-27 10:03:59 -0500

    Added .cache to gitignore and removed scripts directory

---
+++ build/CMakeFiles/chessboard.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/joel/src/cpp/JChessV2/src/common/chessboard/BoardRep.cpp" "CMakeFiles/chessboard.dir/src/common/chessboard/BoardRep.cpp.o" "gcc" "CMakeFiles/chessboard.dir/src/common/chessboard/BoardRep.cpp.o.d"
+  "/home/joel/src/cpp/JChessV2/src/common/chessboard/dir_table.cpp" "CMakeFiles/chessboard.dir/src/common/chessboard/dir_table.cpp.o" "gcc" "CMakeFiles/chessboard.dir/src/common/chessboard/dir_table.cpp.o.d"
+  "/home/joel/src/cpp/JChessV2/src/common/chessboard/move_gen/magics.cpp" "CMakeFiles/chessboard.dir/src/common/chessboard/move_gen/magics.cpp.o" "gcc" "CMakeFiles/chessboard.dir/src/common/chessboard/move_gen/magics.cpp.o.d"
+  "/home/joel/src/cpp/JChessV2/src/common/chessboard/move_gen/move_tables.cpp" "CMakeFiles/chessboard.dir/src/common/chessboard/move_gen/move_tables.cpp.o" "gcc" "CMakeFiles/chessboard.dir/src/common/chessboard/move_gen/move_tables.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/chessboard.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chessboard.dir/src/common/chessboard/BoardRep.cpp.o"
+  "CMakeFiles/chessboard.dir/src/common/chessboard/BoardRep.cpp.o.d"
+  "CMakeFiles/chessboard.dir/src/common/chessboard/dir_table.cpp.o"
+  "CMakeFiles/chessboard.dir/src/common/chessboard/dir_table.cpp.o.d"
+  "CMakeFiles/chessboard.dir/src/common/chessboard/move_gen/magics.cpp.o"
+  "CMakeFiles/chessboard.dir/src/common/chessboard/move_gen/magics.cpp.o.d"
+  "CMakeFiles/chessboard.dir/src/common/chessboard/move_gen/move_tables.cpp.o"
+  "CMakeFiles/chessboard.dir/src/common/chessboard/move_gen/move_tables.cpp.o.d"
+  "lib/libchessboard.a"
+  "lib/libchessboard.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chessboard.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
