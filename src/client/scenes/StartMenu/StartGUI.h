e3b3449e7aacf72a0a82069085cdca017057805f
Joel Kuehne <joelckuehne@gmail.com>
2023-08-02 13:22:03 -0500

    Remaking Start Menu Scene

--- src/client/scenes/StartMenu/StartGUI.h
+++ src/client/scenes/StartMenu/StartGUI.h
@@ -1,18 +1,35 @@
-#pragma once
+#ifndef H_START_GUI
+#define H_START_GUI
 
-#include <iostream>
+#include "StartUI.h"
 
+// Window manager and OpenGL function loader
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #include "../../../../include/glad/glad.h" 
 
-#include "StartUI.h"
+// Helpful utils
 #include "../../gl_macros/gl_macros.h"
 #include "../../gl_macros/shader.h"
 #include "../../gl_macros/stb_image.h"
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+
+// Implement function binding.
+// When creating callbacks for OpenGL, I want to specify which object should
+// be edited when the callback is run. This allows me to create a new function
+// from one of my member functions where "this" is locked in to a particular
+// object. Passing it in as a callback will then edit the right variables.
+//
+// This actually isn't that useful as the single window limitation limits
+// us to one GUI anyway, but I'm just screwing around at this point
+// 
+// Theoretically, one could pass a window as an input to this class and allow
+// two GUI's to be run at once, but that would require passing initialization
+// data in as a void* because this is virtual, and I don't feel like doing
+// that at the moment.
+#include <binders.h>
 
 class StartGUI : public StartUI
 {
@@ -22,79 +39,6 @@ private:
 	virtual void switch_selected_choice(int choice_change);
 	virtual int handle_enter();
 	void draw();
-	
-	/*float vertices[32] = {
-		0.5f, 0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  1.0f, 1.0f, // TR
-		0.5f,-0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  1.0f, 0.0f, // BR
-	       -0.5f,-0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  0.0f, 0.0f, // BL
-	       -0.5f, 0.5f, 0.0f,  1.0f, 1.0f, 0.0f,  0.0f, 1.0f  // TL 
-	};*/
-	float vertices[180] = {
-	       -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-		0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-	       -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-	       -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-
-	       -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-		0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-	       -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-	       -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-
-	       -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-	       -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-	       -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-	       -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-	       -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-	       -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-	       -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-	       -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-	       -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-
-	       -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-	       -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-	       -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
-	};
-	unsigned int indices[6] = {
-		0, 1, 3,
-		1, 2, 3
-	};
-	glm::vec3 cubePositions[10] = {
-		glm::vec3( 0.0f,  0.0f,  0.0f), 
-		glm::vec3( 2.0f,  5.0f, -15.0f), 
-		glm::vec3(-1.5f, -2.2f, -2.5f),  
-		glm::vec3(-3.8f, -2.0f, -12.3f),  
-		glm::vec3( 2.4f, -0.4f, -3.5f),  
-		glm::vec3(-1.7f,  3.0f, -7.5f),  
-		glm::vec3( 1.3f, -2.0f, -2.5f),  
-		glm::vec3( 1.5f,  2.0f, -2.5f), 
-		glm::vec3( 1.5f,  0.2f, -1.5f), 
-		glm::vec3(-1.3f,  1.0f, -1.5f)  
-	};
-
-	GLuint VBO, VAO, EBO;
-	unsigned int texture, texture_2;
-
-	Shader *default_shader;
-
-	float deltaTime, lastFrame;
 
 public:
 	StartGUI();
@@ -105,3 +49,5 @@ public:
 
 	int selected_choices[3] = {0, 0, 0};
 };
+
+#endif
