276d552a3921a6c61fd39d1bc32533074815a730
Joel Kuehne <joelckuehne@gmail.com>
2023-08-13 17:22:52 -0500

    Switched to new models for chess board in StartGUI

--- build/bin/resources/client/StartMenu/fragment_shader.glsl
+++ build/bin/resources/client/StartMenu/fragment_shader.glsl
@@ -1,13 +1,15 @@
 #version 450 core
 out vec4 FragColor;
 
-in vec2 TexCoord;
+in vec2 TexCoords;
+in vec3 color;
 
-uniform sampler2D texture1;
-uniform sampler2D texture2;
+uniform sampler2D texture_diffuse1;
 
 void main()
-{
-	FragColor = mix(texture(texture1, TexCoord),
-		texture(texture2, TexCoord), 0.2);
+{    
+//	FragColor = texture(texture_diffuse1, TexCoords);
+	FragColor.xyz = vec3(0.5f, 0.5f, 0.5f) + 0.5f * color;
+	FragColor.w = 1.0f;
+//	FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);
 }
--- build/bin/resources/client/StartMenu/vertex_shader.glsl
+++ build/bin/resources/client/StartMenu/vertex_shader.glsl
@@ -1,9 +1,10 @@
 #version 450 core
 layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec2 aTexCoord;
+layout (location = 1) in vec3 aNorm;
+layout (location = 2) in vec2 aTexCoord;
 
-out vec3 ourColor;
-out vec2 TexCoord;
+out vec2 TexCoords;
+out vec3 color;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -12,5 +13,8 @@ uniform mat4 projection;
 void main()
 {
 	gl_Position = projection * view * model * vec4(aPos, 1.0);
-	TexCoord = aTexCoord;
+	float intensity = dot(aNorm, normalize(vec3(1.0, 1.0, 1.0)));
+	//color = aNorm * vec3(1.0, 1.0, 1.0);
+	color = vec3(intensity, intensity, intensity);
+	TexCoords = aTexCoord;
 }
